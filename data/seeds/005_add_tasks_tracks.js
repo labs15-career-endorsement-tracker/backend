626f7d623e16551ddd94a884df92562aa568fcc4
clem9281 <clem9281@gmail.com>
2019-08-23 12:50:21 -0700

    completed seeds for tasks_tracks table

--- data/seeds/005_add_tasks_tracks.js
+++ data/seeds/005_add_tasks_tracks.js
@@ -4,6 +4,22 @@ exports.seed = function(knex) {
         { tasks_id: 1, tracks_id: 2 },
         { tasks_id: 1, tracks_id: 3 },
         { tasks_id: 1, tracks_id: 4 },
-        { tasks_id: 1, tracks_id: 5 }
+        { tasks_id: 1, tracks_id: 5 },
+        { tasks_id: 2, tracks_id: 2 },
+        { tasks_id: 2, tracks_id: 4 },
+        { tasks_id: 3, tracks_id: 1 },
+        { tasks_id: 3, tracks_id: 2 },
+        { tasks_id: 3, tracks_id: 3 },
+        { tasks_id: 3, tracks_id: 4 },
+        { tasks_id: 3, tracks_id: 5 },
+        { tasks_id: 4, tracks_id: 1 },
+        { tasks_id: 4, tracks_id: 2 },
+        { tasks_id: 4, tracks_id: 3 },
+        { tasks_id: 4, tracks_id: 4 },
+        { tasks_id: 4, tracks_id: 5 },
+        { tasks_id: 5, tracks_id: 1 },
+        { tasks_id: 5, tracks_id: 2 },
+        { tasks_id: 5, tracks_id: 3 },
+        { tasks_id: 5, tracks_id: 4 }
     ])
 }
