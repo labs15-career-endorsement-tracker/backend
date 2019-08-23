0fffd12604300d6bab28884acbbe7adbe9055b95
clem9281 <clem9281@gmail.com>
2019-08-23 10:57:48 -0700

    started writing seeds for tasks

---
+++ src/data/seeds/003_add_tasks.js
@@ -0,0 +1,33 @@
+exports.seed = function(knex) {
+    return knex("tasks").insert([
+        {
+            title: "Update Resume",
+            is_required: true,
+            description:
+                "Update your resume to include your recent work history",
+            is_endorsement_requirement: true
+        },
+        {
+            title: "Deploy an app to app store",
+            is_required: true,
+            tracks_id: 2,
+            description:
+                "You must have an app deployed on the app store in order to be career endorsed",
+            is_endorsement_requirement: true
+        },
+        {
+            title: "Networking Strategies",
+            is_required: true,
+            description:
+                "80% of jobs are found through networking, so students should be finding opportunities to build their professional networks throughout their time as a student at Lambda School",
+            is_endorsement_requirement: false
+        },
+        {
+            title: "Groom Your Social Media",
+            is_required: false,
+            description:
+                "Groom your Twitter, Facebook, Instagram, or any other public social media profiles:- Remove any photos or posts that contain content that you would not want a potential employer to see.",
+            is_endorsement_requirement: false
+        }
+    ])
+}
