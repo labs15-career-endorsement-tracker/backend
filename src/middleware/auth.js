124388b57165d2bfc6336f4e2dd869c3ea9f1467
clem9281 <clem9281@gmail.com>
2019-08-26 10:53:41 -0700

    took out extraneous console.log()

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -4,7 +4,6 @@ const { Unauthorized } = require("http-errors")
 const auth = (req, res, next) => {
     const token = req.headers.authorization
     const error401 = Unauthorized(`Bad or no token`)
-    console.log(token)
     jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
         if (err) {
             throw error401
