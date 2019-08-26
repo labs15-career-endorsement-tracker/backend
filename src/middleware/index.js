cf7dad8e8e98e3cf0b24470d5c0312412b786a21
Miikis <28997062+Miikis@users.noreply.github.com>
2019-08-26 04:40:58 -0700

    Extract error-handlers into their own file
    Signed-off-by: Miikis <28997062+Miikis@users.noreply.github.com>

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -1,28 +1,9 @@
-const { NotFound } = require("http-errors")
-const handle404 = (req, _res, next) => {
-    const { method, path } = req
-    const msg = `${method} ${path} has not been implemented.`
-    next(NotFound(msg))
-}
-
-const handle500 = ({ status = 500, name, message }, _req, res, next) => {
-    if (res.headersSent) return next()
-    res.status(status).json({ name, statusCode: status, message })
-}
-
-const checkEmailPasswordExist = (req, res, next) => {
-    const { email, password } = req.body
-    if (!email || !password) {
-        res.status(400).json({
-            message: "Please enter your email and password"
-        })
-    } else {
-        next()
-    }
-}
+const { handle404, handle500 } = require("./error-handlers")
+const { validateEmail, validatePassword } = require("./validators")
 
 module.exports = {
     handle404,
     handle500,
-    checkEmailPasswordExist
+    validateEmail,
+    validatePassword
 }
