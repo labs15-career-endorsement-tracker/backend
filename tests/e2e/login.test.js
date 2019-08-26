17c6338d3c002df2d71d4923bd28ac5939c6890a
Miikis <28997062+Miikis@users.noreply.github.com>
2019-08-26 04:44:34 -0700

    Write robust e2e tests for login route
    Signed-off-by: Miikis <28997062+Miikis@users.noreply.github.com>

--- tests/e2e/login.test.js
+++ tests/e2e/login.test.js
@@ -1,43 +1,85 @@
 const request = require("supertest")
-const bcrypt = require("bcryptjs")
 
 const { version } = require("../../src/config")
 const db = require("../../data")
 const app = require("../../src/app")
+const { fakeUsers } = require("../fixtures")
 
 describe("POST /login", () => {
-    beforeEach(done => {
-        db.migrate
-            .latest()
-            .then(() =>
-                db("tracks").insert({
-                    title: "Web"
-                })
-            )
-            .then(() =>
-                db("users").insert({
-                    first_name: "bob",
-                    last_name: "ross",
-                    email: "bob_ross@happylittlemistakes.com",
-                    password: bcrypt.hashSync("Password1234!", 4),
-                    tracks_id: 1
-                })
-            )
-            .then(() => done())
-    })
-
-    afterEach(done => {
-        db.destroy()
+    beforeAll(async done => {
+        await db.migrate.rollback(null, true)
+        await db.migrate.latest()
+        await db("tracks").insert({ title: "Web" })
+        await db("users").insert(fakeUsers)
         done()
     })
-    it("should return a status 200", done => {
+
+    beforeEach(async done => {
+        done()
+    })
+
+    afterAll(async done => {
+        await db.destroy()
+        done()
+    })
+
+    it("returns jwt after successful login w/ email & password", done => {
+        request(app)
+            .post(`/api/v${version}/login`)
+            .send({ email: fakeUsers[0].email, password: "Password1234!" })
+            .expect(200)
+            .then(res => {
+                expect(res.body).toHaveProperty("token")
+                expect(res.body).toHaveProperty("userId")
+                done()
+            })
+    })
+
+    it("throws 400 Error when email is invalid", done => {
         request(app)
             .post(`/api/v${version}/login`)
             .send({
-                email: "bob_ross@happylittlemistakes.com",
+                email: "invalid_email",
                 password: "Password1234!"
             })
-            .expect(200)
-            .then(() => done())
+            .expect(400)
+            .then(res => {
+                expect(res.body).toHaveProperty("message")
+                expect(res.body.name).toEqual("BadRequestError")
+                expect(res.body.statusCode).toEqual(400)
+                done()
+            })
+    })
+
+    it("throws 400 Error when password is invalid", done => {
+        request(app)
+            .post(`/api/v${version}/login`)
+            .send({
+                email: fakeUsers[0].email,
+                password: "short"
+            })
+            .expect(400)
+            .then(res => {
+                expect(res.body).toHaveProperty("message")
+                expect(res.body.name).toEqual("BadRequestError")
+                expect(res.body.statusCode).toEqual(400)
+                done()
+            })
+    })
+
+    it("throws 401 Error when email and/or password are incorrect", done => {
+        request(app)
+            .post(`/api/v${version}/login`)
+            .send({
+                email: fakeUsers[0].email,
+                password: "ValidIncorrect!"
+            })
+            .expect(401)
+            .then(res => {
+                expect(res.body.message).toEqual("Incorrect credentials")
+                expect(res.body.name).toEqual("UnauthorizedError")
+                expect(res.body.statusCode).toEqual(401)
+                done()
+            })
     })
 })
