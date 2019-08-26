8e604d877857fc8b2bf5fd5fac759ec9ee1b4db0
Miikis <28997062+Miikis@users.noreply.github.com>
2019-08-26 06:48:17 -0700

    Add e2e test for duplicate key error
    Signed-off-by: Miikis <28997062+Miikis@users.noreply.github.com>

--- tests/e2e/add-user.test.js
+++ tests/e2e/add-user.test.js
@@ -6,7 +6,7 @@ const app = require("../../src/app")
 const { fakeUsers } = require("../fixtures")
 
 describe("POST /users", () => {
-    beforeAll(async done => {
+    beforeEach(async done => {
         await db.migrate.rollback(null, true)
         await db.migrate.latest()
         await db("tracks").insert({ title: "Web" })
@@ -30,6 +30,27 @@ describe("POST /users", () => {
             .then(res => {
                 const user = res.body
                 expect(user).toHaveProperty("userId")
+                done()
+            })
+    })
+
+    it("throws a 409 error if user already exists", async done => {
+        const bob = fakeUsers[0]
+        bob.password = "Password1234!"
+
+        await db("users").insert(fakeUsers)
+
+        request(app)
+            .post(`/api/v${version}/users`)
+            .send(bob)
+            .set("Accept", "application/json")
+            .expect(409)
+            .then(res => {
+                expect(res.body).toEqual({
+                    message: "User already has an account",
+                    name: "ConflictError",
+                    statusCode: 409
+                })
                 done()
             })
     })
