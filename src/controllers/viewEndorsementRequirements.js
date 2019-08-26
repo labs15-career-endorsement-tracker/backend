87afd76197be3d45a1e335733c8d96005f2b8ebd
clem9281 <clem9281@gmail.com>
2019-08-26 14:05:16 -0700

    added model and controller to return a users requirements based on their track, with steps and progress

--- src/controllers/viewEndorsementRequirements.js
+++ src/controllers/viewEndorsementRequirements.js
@@ -1,13 +1,15 @@
-const { findTasksByTrack, findUsersBy } = require("../model")
+const { getRequirementsWithProgress, findUsersBy } = require("../model")
 
 const viewEndorsementRequirements = async (req, res, next) => {
     const id = req.userId
     try {
         const { tracks_id } = await findUsersBy({ id }).first()
-        const requirements = await findTasksByTrack(tracks_id, id)
-        // console.log(requirements)
-    } catch (error) {}
-    res.json({ id })
+        const requirements = await getRequirementsWithProgress(tracks_id, id)
+        res.json({ requirements })
+    } catch (error) {
+        console.log(error)
+        next(error)
+    }
 }
 
 module.exports = [viewEndorsementRequirements]
--- src/model/index.js
+++ src/model/index.js
@@ -4,28 +4,59 @@ const findUsers = () => db("users")
 
 const findUsersBy = filter => db("users").where(filter)
 
+// Get all the steps for a task
 const findStepsByTask = taskId => db("steps").where({ tasks_id: taskId })
 
+// Get steps from the user_steps_completed table either by user_id or steps_id
 const findCompletedStepsBy = filter => db("user_steps_completed").where(filter)
 
-const findTasksByTrack = async (trackId, userId) => {
-    let userCompletedSteps = await findCompletedStepsBy({
-        user_id: userId
-    }).map(el => el.steps_id)
-    console.log(userCompletedSteps)
-    const allRequirements = await db("tasks_tracks")
+// get all the requirements (NOTE: REQUIREMENTS NOT INCLUDING ASSIGNMENTS) for a track
+const findRequirementsByTrack = trackId =>
+    db("tasks_tracks")
         .join("tasks", "tasks.id", "tasks_tracks.tasks_id")
         .where({ tracks_id: trackId })
         .andWhere({ is_endorsement_requirement: true })
+
+const formatRequirementsWithSteps = async (
+    allRequirements,
+    userCompletedSteps
+) => {
     const allRequirementsWithSteps = await Promise.all(
         allRequirements.map(async el => {
+            // get all the steps for a given task
             const steps = await findStepsByTask(el.tasks_id)
-
-            return { ...el, steps: [...steps] }
+            // give each step a completed flag
+            const stepsWithFlags = steps.map(step =>
+                userCompletedSteps.includes(step.id)
+                    ? { ...step, completed: true }
+                    : { ...step, completed: false }
+            )
+            // get just the completed steps for this task for progress average
+            const justCompletedStepsForThisTask = stepsWithFlags.filter(
+                el => el.completed
+            )
+            // calculate progress
+            const progress =
+                (justCompletedStepsForThisTask.length / steps.length) * 100
+            return { ...el, progress, steps: [...stepsWithFlags] }
         })
     )
-    console.log(1, allRequirementsWithSteps)
-    return allRequirements
+    return allRequirementsWithSteps
 }
 
-module.exports = { findUsers, findUsersBy, findTasksByTrack }
+const getRequirementsWithProgress = async (trackId, userId) => {
+    // get all the steps a user has completed
+    const userCompletedSteps = await findCompletedStepsBy({
+        user_id: userId
+    }).map(el => el.steps_id)
+    // get all the requirements (NOTE: REQUIREMENTS NOT INCLUDING ASSIGNMENTS) for a track
+    const allRequirements = await findRequirementsByTrack(trackId)
+    // map over each requirement and get its steps. compare with the steps a user has completed to add a completed flag to each step
+    const allRequirementsWithSteps = formatRequirementsWithSteps(
+        allRequirements,
+        userCompletedSteps
+    )
+    return allRequirementsWithSteps
+}
+
+module.exports = { findUsers, findUsersBy, getRequirementsWithProgress }
