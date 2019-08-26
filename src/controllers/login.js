9c4e13b9b5d5861de7f1e08982cd319495a75f5f
Miikis <28997062+Miikis@users.noreply.github.com>
2019-08-26 03:28:10 -0700

    Refactor login controller
    - Change password comparison to be asynchronous
    - Utilize `http-errors` library and express error-handling middleware for http errors
    - Remove `console.error` statement. If we left it in, it would show up on automated tests and it's not like we can't throw it in ad hoc if we need to.
    - Change jwt payload to be just `userId` and also send back a `userId` for the client.
    
    Signed-off-by: Miikis <28997062+Miikis@users.noreply.github.com>

--- src/controllers/login.js
+++ src/controllers/login.js
@@ -1,4 +1,5 @@
-const bcrypt = require("bcryptjs")
+const { compare } = require("bcryptjs")
+const { Unauthorized } = require("http-errors")
 
 const { findUsersBy } = require("../model")
 const { generateJwt } = require("../utils")
@@ -8,23 +9,17 @@ const login = async (req, res, next) => {
 
     try {
         const user = await findUsersBy({ email }).first()
+        const error401 = Unauthorized(`Incorrect credentials`)
 
-        if (user && bcrypt.compareSync(password, user.password)) {
-            const token = generateJwt({
-                userId: user.id,
-                isAdmin: user.is_admin
-            })
-            res.status(200).json({
-                message: `Welcome ${user.first_name}, you are logged in`,
-                token
-            })
-        } else {
-            res.status(401).json({
-                errorMessage: "That email or password is not correct."
-            })
-        }
+        if (!user) throw error401
+
+        const isCorrectPassword = await compare(password, user.password)
+
+        if (!isCorrectPassword) throw error401
+
+        const userId = user.id
+        res.json({ token: generateJwt({ userId }), userId })
     } catch (error) {
-        console.error(error)
         next(error)
     }
 }
