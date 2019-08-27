99bd7728022cb5244a27c07539ec714f22afdba4
clem9281 <clem9281@gmail.com>
2019-08-27 16:56:14 -0700

    fixed failing test

--- tests/fixtures/index.js
+++ tests/fixtures/index.js
@@ -6,7 +6,8 @@ const fakeUsers = [
         last_name: "ross",
         email: "bob_ross@happylittlemistakes.com",
         password: bcrypt.hashSync("Password1234!", 4),
-        tracks_id: 1
+        tracks_id: 1,
+        is_admin: false
     }
 ]
 const fakeTracks = [{ title: "Web" }]
