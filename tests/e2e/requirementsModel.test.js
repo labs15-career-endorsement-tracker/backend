40b3f134059b3e25fd44f4f4f516b91a0f03a498
clem9281 <clem9281@gmail.com>
2019-08-27 10:27:03 -0700

    finished writing tests for /view-requirements: TESTED ready fir review

--- tests/e2e/requirementsModel.test.js
+++ tests/e2e/requirementsModel.test.js
@@ -37,44 +37,41 @@ describe("MODEL requirements", () => {
     describe("findRequirementsByTrack", () => {
         // the only track_id we have is 1
         it("should return an array of objects", done => {
-            findRequirementsByTrack(1)
-                .then(res => {
-                    expect(res).toEqual(expect.any(Array))
-                    expect(res).toContainEqual(expect.any(Object))
-                })
-                .then(() => done())
+            findRequirementsByTrack(1).then(res => {
+                expect(res).toEqual(expect.any(Array))
+                expect(res).toContainEqual(expect.any(Object))
+                done()
+            })
         })
         it("should return an array of objects with shape: {id (int), is_endorsement_requirement (bool), is_required (bool), tasks_id (int), title (string), tracks_id (int),  tasks_description (String)} ", done => {
-            findRequirementsByTrack(1)
-                .then(res => {
-                    expect(res[0]).toEqual(
-                        expect.objectContaining({
-                            id: expect.any(Number),
-                            is_endorsement_requirement: expect.any(Boolean),
-                            is_required: expect.any(Boolean),
-                            tasks_id: expect.any(Number),
-                            title: expect.any(String),
-                            tracks_id: expect.any(Number),
-                            tasks_description: expect.any(String)
-                        })
-                    )
-                })
-                .then(() => done())
+            findRequirementsByTrack(1).then(res => {
+                expect(res[0]).toEqual(
+                    expect.objectContaining({
+                        id: expect.any(Number),
+                        is_endorsement_requirement: expect.any(Boolean),
+                        is_required: expect.any(Boolean),
+                        tasks_id: expect.any(Number),
+                        title: expect.any(String),
+                        tracks_id: expect.any(Number),
+                        tasks_description: expect.any(String)
+                    })
+                )
+                done()
+            })
         })
         it("should have this object as it's first element: {title: 'Requirement 1',is_required: true, tasks_description: 'Requirement 1 description',is_endorsement_requirement: true },", done => {
-            findRequirementsByTrack(1)
-                .then(res => {
-                    expect(res[0]).toEqual({
-                        id: 1,
-                        is_endorsement_requirement: true,
-                        is_required: true,
-                        tasks_id: 1,
-                        title: "Requirement 1",
-                        tracks_id: 1,
-                        tasks_description: "Requirement 1 description"
-                    })
+            findRequirementsByTrack(1).then(res => {
+                expect(res[0]).toEqual({
+                    id: 1,
+                    is_endorsement_requirement: true,
+                    is_required: true,
+                    tasks_id: 1,
+                    title: "Requirement 1",
+                    tracks_id: 1,
+                    tasks_description: "Requirement 1 description"
                 })
-                .then(() => done())
+                done()
+            })
         })
     })
 })
--- tests/e2e/viewEndorsementRequirements.test.js
+++ tests/e2e/viewEndorsementRequirements.test.js
@@ -63,7 +63,7 @@ describe("GET /view-requirements", () => {
             .expect(200)
             .then(res => done())
     })
-    it("should return an array", done => {
+    it("should return an array of objects", done => {
         return request(app)
             .get(`/api/v${version}/view-requirements`)
             .set("Accept", "application/json")
@@ -72,6 +72,49 @@ describe("GET /view-requirements", () => {
             .expect(200)
             .then(res => {
                 expect(res.body).toEqual(expect.any(Array))
+                expect(res.body).toContainEqual(expect.any(Object))
+                done()
+            })
+    })
+    it("should return an array of objects with shape: {id (int), is_endorsement_requirement (bool), is_required (bool), tasks_id (int), title (string), tracks_id (int),  tasks_description (String)} ", done => {
+        return request(app)
+            .get(`/api/v${version}/view-requirements`)
+            .set("Accept", "application/json")
+            .set("authorization", `bearer ${token}`)
+            .expect("Content-Type", /json/)
+            .expect(200)
+            .then(res => {
+                expect(res.body[0]).toEqual(
+                    expect.objectContaining({
+                        id: expect.any(Number),
+                        is_endorsement_requirement: expect.any(Boolean),
+                        is_required: expect.any(Boolean),
+                        tasks_id: expect.any(Number),
+                        title: expect.any(String),
+                        tracks_id: expect.any(Number),
+                        tasks_description: expect.any(String)
+                    })
+                )
+                done()
+            })
+    })
+    it("should have this object as it's first element: {title: 'Requirement 1',is_required: true, tasks_description: 'Requirement 1 description',is_endorsement_requirement: true },", done => {
+        return request(app)
+            .get(`/api/v${version}/view-requirements`)
+            .set("Accept", "application/json")
+            .set("authorization", `bearer ${token}`)
+            .expect("Content-Type", /json/)
+            .expect(200)
+            .then(res => {
+                expect(res.body[0]).toEqual({
+                    id: 1,
+                    is_endorsement_requirement: true,
+                    is_required: true,
+                    tasks_id: 1,
+                    title: "Requirement 1",
+                    tracks_id: 1,
+                    tasks_description: "Requirement 1 description"
+                })
                 done()
             })
     })
