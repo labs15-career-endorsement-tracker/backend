b0424703a4f3aa441a80ad03c0b4b3863902678f
clem9281 <clem9281@gmail.com>
2019-08-27 15:13:14 -0700

    removed references to functions that no longer exist

--- src/model/index.js
+++ src/model/index.js
@@ -1,10 +1,6 @@
 const db = require("../../data")
-const {
-    findRequirementsByTrack,
-    getRequirementsWithProgress
-} = require("./requirements")
+const { findRequirementsByTrack } = require("./requirements")
 
-const { findCompletedStepsBy, findStepsByTask } = require("./steps")
 const { findAllTracks } = require("./tracks")
 
 const findUsers = () => db("users")
@@ -15,6 +11,5 @@ module.exports = {
     findUsers,
     findUsersBy,
     findRequirementsByTrack,
-    getRequirementsWithProgress,
     findAllTracks
 }
