454ba1f1f1287c76fb841de1bf3e28d98458d5a1
clem9281 <clem9281@gmail.com>
2019-08-27 14:31:09 -0700

    finished tests for model

--- tests/e2e/tasksModel.test.js
+++ tests/e2e/tasksModel.test.js
@@ -36,9 +36,9 @@ describe("MODEL requirements", () => {
                 done()
             })
         })
-        it("should have [{'id': 1, 'title': 'Web'}] as it's first element,", done => {
-            findRequirementsByTrack(1).then(res => {
-                expect(res[0]).toEqual([{ id: 1, title: "Web" }])
+        it("should have {'id': 1, 'title': 'Web'} as it's first element,", done => {
+            findAllTracks().then(res => {
+                expect(res[0]).toEqual({ id: 1, title: "Web" })
                 done()
             })
         })
