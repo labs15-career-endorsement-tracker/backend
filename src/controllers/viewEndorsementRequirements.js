23216f34213f3aa8b2d2be23d85daa324921a9d6
clem9281 <clem9281@gmail.com>
2019-08-27 08:59:21 -0700

    refactored requirements to remove extraneous code: now the view-requirements endpoint just shows the requirements for that user's track

--- src/controllers/viewEndorsementRequirements.js
+++ src/controllers/viewEndorsementRequirements.js
@@ -1,10 +1,10 @@
-const { getRequirementsWithProgress, findUsersBy } = require("../model")
+const { findRequirementsByTrack, findUsersBy } = require("../model")
 
 const viewEndorsementRequirements = async (req, res, next) => {
     const id = res.locals.userId
     try {
         const { tracks_id } = await findUsersBy({ id }).first()
-        const requirements = await getRequirementsWithProgress(tracks_id, id)
+        const requirements = await findRequirementsByTrack(tracks_id)
         res.json({ requirements })
     } catch (error) {
         console.log(error)
--- src/model/requirements.js
+++ src/model/requirements.js
@@ -1,5 +1,5 @@
 const db = require("../../data")
-const { findCompletedStepsBy, findStepsByTask } = require("./steps")
+
 // get all the requirements (NOTE: REQUIREMENTS NOT INCLUDING ASSIGNMENTS) for a track
 const findRequirementsByTrack = trackId =>
     db("tasks_tracks")
@@ -7,46 +7,4 @@ const findRequirementsByTrack = trackId =>
         .where({ tracks_id: trackId })
         .andWhere({ is_endorsement_requirement: true })
 
-const formatRequirementsWithSteps = async (
-    allRequirements,
-    userCompletedSteps
-) => {
-    const allRequirementsWithSteps = await Promise.all(
-        allRequirements.map(async el => {
-            // get all the steps for a given task
-            const steps = await findStepsByTask(el.tasks_id)
-            // give each step a completed flag
-            const stepsWithFlags = steps.map(step =>
-                userCompletedSteps.includes(step.id)
-                    ? { ...step, completed: true }
-                    : { ...step, completed: false }
-            )
-            // get just the completed steps for this task for progress average
-            const justCompletedStepsForThisTask = stepsWithFlags.filter(
-                el => el.completed
-            )
-            // calculate progress
-            const progress =
-                (justCompletedStepsForThisTask.length / steps.length) * 100
-            return { ...el, progress, steps: [...stepsWithFlags] }
-        })
-    )
-    return allRequirementsWithSteps
-}
-
-const getRequirementsWithProgress = async (trackId, userId) => {
-    // get all the steps a user has completed
-    const userCompletedSteps = await findCompletedStepsBy({
-        user_id: userId
-    }).map(el => el.steps_id)
-    // get all the requirements (NOTE: REQUIREMENTS NOT INCLUDING ASSIGNMENTS) for a track
-    const allRequirements = await findRequirementsByTrack(trackId)
-    // map over each requirement and get its steps. compare with the steps a user has completed to add a completed flag to each step
-    const allRequirementsWithSteps = formatRequirementsWithSteps(
-        allRequirements,
-        userCompletedSteps
-    )
-    return allRequirementsWithSteps
-}
-
-module.exports = { findRequirementsByTrack, getRequirementsWithProgress }
+module.exports = { findRequirementsByTrack }
