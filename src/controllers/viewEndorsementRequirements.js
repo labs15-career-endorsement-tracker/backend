8e0578819b42af4269117c2211dfc98784c49760
clem9281 <clem9281@gmail.com>
2019-08-30 16:54:18 -0700

    updated endpoint tests for /requirements endpoint

--- src/controllers/viewEndorsementRequirements.js
+++ src/controllers/viewEndorsementRequirements.js
@@ -1,10 +1,14 @@
-const { findRequirementsByTrack, findUsersBy } = require("../model")
+const {
+    findRequirementsByTrack,
+    findUsersBy,
+    getRequirementsWithProgress
+} = require("../model")
 
 const viewEndorsementRequirements = async (req, res, next) => {
     const id = res.locals.userId
     try {
         const { tracks_id } = await findUsersBy({ id }).first()
-        const requirements = await findRequirementsByTrack(tracks_id)
+        const requirements = await getRequirementsWithProgress(id, tracks_id)
         res.json(requirements)
     } catch (error) {
         console.log(error)
