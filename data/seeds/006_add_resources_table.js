5ef49cabede91d01fbff0c7949227e7821bf16e4
clem9281 <clem9281@gmail.com>
2019-09-04 09:59:04 -0700

    adds resume seeds

--- data/seeds/006_add_resources_table.js
+++ data/seeds/006_add_resources_table.js
@@ -8,7 +8,7 @@ exports.seed = function(knex) {
                 "https://docs.google.com/document/d/1wZkDPBWtQZDGGdvStD61iRx_jOWVlIyyQl9UOYHtZgA/edit"
         },
         {
-            type: "google_doc",
+            type: "unspecified",
             title: "Power statements article",
             tasks_id: 1,
             url:
@@ -20,6 +20,48 @@ exports.seed = function(knex) {
             tasks_id: 1,
             url:
                 "https://docs.google.com/document/d/19OxIgJYkLMq4c1o5zHu1Na4a3PYcyutOosVfg6a03RI/edit"
+        },
+        {
+            type: "google_doc",
+            title: "Resume Deep Dive",
+            tasks_id: 1,
+            url:
+                "https://docs.google.com/document/d/1BOwmlkiPJH_96dnjI3nAW8ok-BOR8hkl5Us70bwf24U/edit"
+        },
+        {
+            type: "unspecified",
+            title: "Resume  Rubric",
+            tasks_id: 1,
+            url:
+                "https://drive.google.com/file/d/1h0CCENtwDuNxVwqIG8-P6O2sZj8S4BPg/view"
+        },
+        {
+            type: "google_doc",
+            title: "Resume tips for UX Designers",
+            tasks_id: 1,
+            url:
+                "https://docs.google.com/document/d/1yHAw9CbXjmA5TJwI8j84uS-w5tojC8y7eeoGmmoYzMc/edit"
+        },
+        {
+            type: "google_doc",
+            title: "Resume tips for Mobile Developers",
+            tasks_id: 1,
+            url:
+                "https://docs.google.com/document/d/1bvqn0aFGpvkaKVfmBDAhtmoWlAlDoRTD5DuiMXwsJlQ/edit"
+        },
+        {
+            type: "google_doc",
+            title: "Resume tips for Web Developers",
+            tasks_id: 1,
+            url:
+                "https://docs.google.com/document/d/1RHpYdEqrvSPL3Z4dIHLVaYyju06aGrApBI6RMyAqFg0/edit"
+        },
+        {
+            type: "google_doc",
+            title: "Resume tips for Data Scientists",
+            tasks_id: 1,
+            url:
+                "https://docs.google.com/document/d/1bYAuFyZfa-BIAYUf529Px3ULNOKZwAvUFRxDqlOCvaE/edit"
         },
         {
             type: "youtube_vid",
