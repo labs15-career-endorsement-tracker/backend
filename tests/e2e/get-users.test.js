97caea9817daf63efd0f8ce1639a92671ec7c3fe
clem9281 <clem9281@gmail.com>
2019-09-06 08:59:08 -0700

    adds requires auth to get /users

--- tests/e2e/get-users.test.js
+++ tests/e2e/get-users.test.js
@@ -6,11 +6,21 @@ const app = require("../../src/app")
 const { fakeUsers } = require("../fixtures")
 
 describe("GET /users", () => {
+    let token
     beforeAll(async done => {
         await db.migrate.rollback(null, true)
         await db.migrate.latest()
         await db("tracks").insert({ title: "Web" })
         await db("users").insert(fakeUsers)
+        // login you need a token
+        const res = await request(app)
+            .post(`/api/v${version}/login`)
+            .send({
+                email: fakeUsers[0].email,
+                password: "Password1234!"
+            })
+        token = res.body.token
+        done()
         done()
     })
     beforeEach(async done => {
@@ -24,6 +34,7 @@ describe("GET /users", () => {
     it("returns a list of users", done => {
         request(app)
             .get(`/api/v${version}/users`)
+            .set("authorization", `bearer ${token}`)
             .set("Accept", "application/json")
             .expect("Content-Type", /json/)
             .expect(200)
