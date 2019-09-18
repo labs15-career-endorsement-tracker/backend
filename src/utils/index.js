c8a0779a78e6ff96368e01d4f21d65e0b9abbd34
Adam Mathieson <mathieson1222@gmail.com>
2019-09-17 17:26:04 -0700

    cleans up commented out code in utils

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,27 +1,18 @@
 const jwt = require("jsonwebtoken")
 
-const { jwtSecret, emailConfig } = require("../config")
+const { jwtSecret } = require("../config")
 
 const generateJwt = (payload, secret = jwtSecret, expiresIn = "1d") => {
     return jwt.sign(payload, secret, { expiresIn })
 }
 
 const extractJwt = (token, secret = jwtSecret) => {
-    //    if (secret) {
+    
     return jwt.verify(token, secret, (err, decoded) => {
         if (err) {
             throw err
         }
         return decoded
     })
-    //    } else {
-    //     return jwt.verify(token, emailSecret, (err, decoded) => {
-    //         if (err) {
-    //             throw err
-    //         } else {
-    //             return decoded
-    //         }
-    //     })
-    //    }
 }
 module.exports = { generateJwt, extractJwt }
