575e0399e50b964a1e7832902a0d11f037b28c3a
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-09-19 19:29:43 -0400

    Defines the 'exports.up' and the 'exports.down' for the 'alter_users_table_gin_index.js' file

--- data/migrations/20190919191239_alter_users_table_gin_index.js
+++ data/migrations/20190919191239_alter_users_table_gin_index.js
@@ -1,8 +1,17 @@
+exports.up = async function(knex) {
+    const hasTable = await knex.schema.hasTable("users")
+    if (!hasTable) return
 
-exports.up = function(knex) {
-  
-};
+    return knex.schema.table("users", tbl => {
+        tbl.specificType("full_text", "tsvector")
+
+        tbl.index("full_text", "full_text_index", "GIN")
+    })
+}
 
 exports.down = function(knex) {
-  
-};
+    return knex.schema.table("users", async tbl => {
+        tbl.dropColumn("full_text")
+        tbl.dropIndex("full_text_index")
+    })
+}
