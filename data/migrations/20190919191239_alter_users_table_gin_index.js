35fc5c72bd46bd4a3cd069a66cc63c8e46b9d2ad
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-09-20 00:38:16 -0400

    Mikis went into the matrix with me to solve the errors inside of the errors that were inside the other errors

--- data/migrations/20190919191239_alter_users_table_gin_index.js
+++ data/migrations/20190919191239_alter_users_table_gin_index.js
@@ -1,17 +1,42 @@
+const addUserIndex = `
+ALTER TABLE public.users
+	ADD COLUMN full_text_weighted tsvector;
+UPDATE public.users
+SET full_text_weighted = setweight(to_tsvector(first_name), 'A') 
+    ||  setweight(to_tsvector(last_name), 'B') 
+    || 	setweight(to_tsvector(email), 'C');
+CREATE INDEX full_text_weighted_index
+	ON public.users
+    USING GIN (full_text_weighted);
+    
+CREATE FUNCTION user_tsvector_trigger() RETURNS trigger AS $$
+begin
+    new.full_text_weighted :=     --'new' just means "new row"
+    setweight(to_tsvector('english', coalesce(new.first_name, '')), 'A')
+    || setweight(to_tsvector('english', coalesce(new.last_name, '')), 'B')
+    || setweight(to_tsvector('english', coalesce(new.email, '')), 'C');
+    return new;
+END
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER tsvectorupdate BEFORE INSERT OR UPDATE
+	ON public.users FOR EACH ROW EXECUTE PROCEDURE user_tsvector_trigger();
+`
+
+const removeUserIndex = `
+DROP FUNCTION IF EXISTS user_tsvector_trigger();`
+
 exports.up = async function(knex) {
     const hasTable = await knex.schema.hasTable("users")
     if (!hasTable) return
 
-    return knex.schema.table("users", tbl => {
-        tbl.specificType("full_text", "tsvector")
-
-        tbl.index("full_text", "full_text_index", "GIN")
-    })
+    return knex.schema.raw(addUserIndex)
 }
 
-exports.down = function(knex) {
-    return knex.schema.table("users", async tbl => {
-        tbl.dropColumn("full_text")
-        tbl.dropIndex("full_text_index")
+exports.down = async function(knex) {
+    await knex.schema.table("users", async tbl => {
+        tbl.dropColumn("full_text_weighted")
+        tbl.dropIndex("full_text_weighted", "full_text_weighted_index")
     })
+    return knex.schema.raw(removeUserIndex)
 }
